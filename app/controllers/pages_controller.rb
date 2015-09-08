a29ba5029e0af18ce7f61c379608145023763bb9
Alex Kestner <alex.kestner@gmail.com>
2015-09-08 13:35:01 -0400

    assigned necessary Rails config vars to gon in pages_controller and initialized Gon in application.html.erb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
+    # send data to js via gon
+    gon.rails_env = Rails.env
+    gon.segment_io_key = Rails.application.config.segment_io_key
   end
 end
