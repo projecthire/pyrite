d273cdafd706fa62cf3eb8931e5ca279ba437dda
Alex Kestner <alex.kestner@gmail.com>
2015-09-10 17:06:13 -0400

    adds ContactsController

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,33 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    respond_to do |format|
+      if @contact.save
+        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
+        format.js {}
+        format.json { render json: @contact, status: :created, location: @contact }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email)
+    end
+end
