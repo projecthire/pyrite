e28537f3f6491d0989597c245cc7662e9200e5c7
Alex Kestner <alex.kestner@gmail.com>
2015-09-16 17:41:37 -0400

    adds Candidate and Employer models, sub-classed from User via Single Table Inheritance

---
+++ app/models/employer.rb
@@ -0,0 +1,23 @@
+class Employer < User
+  belongs_to :organization
+  belongs_to :role
+
+  before_save :set_default_role
+
+  def candidate?
+    false
+  end
+
+  def employer?
+    true
+  end
+
+  def owner?
+    self.role.name == 'OrganizationOwner'
+  end
+
+  private
+    def set_default_role
+      self.role ||= Role.find_by_name('OrganizationMember')
+    end
+end
