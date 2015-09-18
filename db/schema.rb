a553a5bbd857d3621e1f929ec10829353a14025d
Alex Kestner <alex.kestner@gmail.com>
2015-09-18 11:44:37 -0400

    adds foreign key on LinkedinAuth to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150916194359) do
+ActiveRecord::Schema.define(version: 20150917211603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,17 @@ ActiveRecord::Schema.define(version: 20150916194359) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "linkedin_auths", force: :cascade do |t|
+    t.string   "linkedin_uid"
+    t.string   "token"
+    t.string   "secret"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "user_id"
+  end
+
+  add_index "linkedin_auths", ["user_id"], name: "index_linkedin_auths_on_user_id", using: :btree
 
   create_table "organizations", force: :cascade do |t|
     t.string   "name"
@@ -80,5 +91,6 @@ ActiveRecord::Schema.define(version: 20150916194359) do
   add_index "users", ["organization_id"], name: "index_users_on_organization_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "linkedin_auths", "users"
   add_foreign_key "users", "organizations"
 end
