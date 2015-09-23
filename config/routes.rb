c29f49802126d6fcd33f79474b5952f1ab1680f3
Alex Kestner <alex.kestner@gmail.com>
2015-09-23 11:21:49 -0400

    adds Candidates::RegistrationController to allow :name param during sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :contacts, only: [:new, :create]
-  devise_for :candidates
+  devise_for :candidates, controllers: {
+    registrations: 'candidates/registrations'
+  }
 
   root "contacts#new"
 end
