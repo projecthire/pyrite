caa4eba5ddc9d27528e36d502fa4f1ae463d9568
Alex Kestner <alex.kestner@gmail.com>
2015-09-23 22:08:33 -0400

    admin subdomain routing for RailsAdmin and Admin model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  constraints subdomain: /admin/ do
+    devise_for :admins,
+               path_names: { sign_in: 'login', sign_out: 'logout' }
+
+    mount RailsAdmin::Engine => '/', as: 'rails_admin'
+  end
+
   resources :contacts, only: [:new, :create]
   devise_for :candidates,
              path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' },
