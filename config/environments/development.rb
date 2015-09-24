a25d257c6616fbe580232573d0154d2f133ca5de
Alex Kestner <alex.kestner@gmail.com>
2015-09-24 10:19:28 -0400

    removes duplicate action_mailer.delivery_method setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,6 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { host: "localhost", port: 5000 }
 
