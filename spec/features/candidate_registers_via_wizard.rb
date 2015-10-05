afb0a64a8c8155d74a8058a4e6ec5bba7f5e5f2a
Alex Kestner <alex.kestner@gmail.com>
2015-10-05 16:57:03 -0400

    adds candidate_registers_via_wizard feature spec

---
+++ spec/features/candidate_registers_via_wizard.rb
@@ -0,0 +1,32 @@
+feature 'Candidate registers' do
+  scenario 'via candidate landing page' do
+    visit root_path
+    click_button 'navbar_candidate_signup_btn'
+
+    # first step of funnel
+    expect(current_path).to eq(registration_wizard_path(:personal))
+
+    fill_in "candidate[name]", with: Faker::Name.name
+    fill_in "candidate[email]", with: Faker::Internet.email
+
+    click_button "Continue"
+
+    # second step of funnel
+    expect(current_path).to eq(registration_wizard_path(:professional))
+
+    select 'mba_student', from: 'candidate[current_profession]'
+    select '3-5', from: 'candidate[years_experience]'
+    select 'hedge_fund', from: 'candidate[desired_profession]'
+    select 'london', from: 'candidate[desired_location]'
+    select 'us_citizen', from: 'candidate[work_status]'
+
+    password = Faker::Internet.password 8
+    fill_in "candidate[password]", with: password
+    fill_in "candidate[password_confirmation]", with: password
+
+    click_button "Continue"
+
+    # third step of funnel
+    expect(current_path).to eq(registration_wizard_path(:authorize))
+  end
+end
