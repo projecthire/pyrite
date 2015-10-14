a203bf0a8c4543d7a2febafba45c14cdba1c8207
Alex Kestner <alex.kestner@gmail.com>
2015-10-14 14:26:26 -0400

    adds Candidate.start_date

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008235907) do
+ActiveRecord::Schema.define(version: 20151014163148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20151008235907) do
     t.integer  "years_experience"
     t.integer  "current_profession"
     t.integer  "work_status"
+    t.datetime "start_date"
   end
 
   add_index "candidates", ["confirmation_token"], name: "index_candidates_on_confirmation_token", unique: true, using: :btree
