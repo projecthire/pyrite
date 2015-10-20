2a1be6e0ab9161e1cd55164001cd449e489f9eb2
Khaled Alquaddoomi <khaled@healthguru.com>
2015-10-20 15:25:20 -0400

    Social Networking footer links and test case

--- spec/features/guest_sees_social_links_spec.rb
+++ spec/features/guest_sees_social_links_spec.rb
@@ -1,8 +1,8 @@
 feature 'Guest sees social links' do
   scenario "observes social links" do
     visit root_path
-    expect(page).to have_link('https://facebook.com/social.projecthire');
-    expect(page).to have_link('https://twitter.com/ProjectHireInc');
-    expect(page).to have_link('https://www.linkedin.com/company/projecthire');
+    expect(page).to have_css "a[href='https://facebook.com/social.projecthire']"
+    expect(page).to have_css "a[href='https://twitter.com/ProjectHireInc']"
+    expect(page).to have_css "a[href='https://www.linkedin.com/company/projecthire']"
   end
 end
